c9c80a5b5011f099538d938a0dd9d241c36c9519
Homer Adriel Dorin <154606101+Peace1820@users.noreply.github.com>
2024-11-08 14:14:00 +0800

    Update additionTableFunction.c

--- PFE Practice/additionTableFunction.c
+++ PFE Practice/additionTableFunction.c
@@ -8,7 +8,6 @@ Ask the user to input an integer n and print the addition table. Limit the value
 Use do-while loop to validate the input. 
 If the input is valid,  call the function to display the addition table using nested for loops.
 
-
 Example:
 
 Enter n: 5
@@ -22,52 +21,44 @@ Addition Table
 4       4       5       6       7       8       9
 5       5       6       7       8       9   
 
-
-
 */
-
 #include <stdio.h>
 
 void additionTable(int);
+
 
 int main(){
     
     int n;
     
     do {
+            
+        printf("Enter n: ");
+        scanf("%d", &n);
     
-    printf("Enter n: ");
-    scanf("%d", &n);
-    
-    
-    } while(n <= 0 || n >= 9);
+    } while (n < 1 || n > 8);
     
     additionTable(n);
 }
 
-void additionTable(int n){
+void additionTable (int n){
     
     printf("\nAddition Table\n");
     printf("\t");
     
-    for(int i = 0; i <= n; i++) {
-    
-        printf("%d\t", i);
-    }
-    
-     printf("\n");
-    
-    for(int j = 0; j <= n; j++) {
-    
-        printf("%d\t", j);
-        
-        for(int k = 0; k <= n; k++) {
-    
-            printf("%d\t", j + k);
-    }
+    for (int i = 0; i <= n; i++) printf("%d\t", i);
     
     printf("\n");
     
+    for (int j = 0; j <= n; j++){
+        
+        printf("%d\t", j);
+        
+        for (int k = 0; k <= n; k++){
+            
+            printf("%d\t", j + k);    
+        }
+        
+        printf("\n");
     }
-    
 }
